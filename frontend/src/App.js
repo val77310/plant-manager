75865eb984c019471254799b6e8c7f2edcdfae7e
Valentin <47883020+val77310@users.noreply.github.com>
2025-08-08 15:18:11 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Plant Manager</h1>
+      <p>Bienvenue dans ton gestionnaire de plantes !</p>
+    </div>
+  );
+}
+
+export default App;
